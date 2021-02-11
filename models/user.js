98e606e5ae7a1a8c10e74971d4ade89a3c6530b9
EmilieGonin <emilie.yuu@gmail.com>
2021-02-11 10:20:22 +0100

    Add unique to userSchema's email + install mongoose-unique-validator

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
 //!minOnSave
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-  userId: {type: String, required: true},
-  email: {type: String, required: true},
-  password: {type: String, required: true}
+  userId: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
 module.exports = mongoose.model("User", userSchema);
