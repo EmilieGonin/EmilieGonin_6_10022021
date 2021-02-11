ce2cdc3678b920a347688ae76180aa57a902cbb3
EmilieGonin <emilie.yuu@gmail.com>
2021-02-11 10:29:52 +0100

    Install bcrypt + Add password hash

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,18 +2,20 @@
 const User = require("../models/user");
 
 exports.userSignup = (req, res, next) => {
-  //Chiffrement mdp
+  //delete req.body._id ?
+  bcrypt.hash(req.body.password, 10)
+  .then(hash => {
+    const user = new User({
+      //userID: req.params.id ?
+      email: req.body.email,
+      password: hash
+    });
 
-  delete req.body._id;
-  const user = new User({
-    //userID: req.params.id ?
-    email: req.body.email
-    //password: hash
-  });
-
-  user.save()
-  .then(() => res.status(201).json({ message: "User added!" }))
-	.catch((error) => res.status(400).json({ error }));
+    user.save()
+    .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+  	.catch((error) => res.status(400).json({ error }));
+  })
+  .catch((error) => res.status(500).json({ error }));
 });
 exports.userLogin = (req, res, next) => {
   //
