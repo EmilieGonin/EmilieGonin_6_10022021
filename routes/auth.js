7a43bfcca5a94e2aee8a4a3f270d37ebc86668ed
EmilieGonin <emilie.yuu@gmail.com>
2021-02-11 09:59:19 +0100

    Add Sauce.find(), Sauce.findOne() and Sauce.deleteOne()

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,21 @@
 //!minOnSave
 const express = require('express');
 const router = express.Router();
-
 const User = require("../models/user");
 
 router.post("/signup", (req, res, next) => {
-  //
+  //Chiffrement mdp
+
+  delete req.body._id;
+  const user = new User({
+    //userID: req.params.id ?
+    email: req.body.email
+    //password: hash
+  });
+
+  user.save()
+  .then(() => res.status(201).json({ message: "User added!" }))
+	.catch((error) => res.status(400).json({ error }));
 });
 router.post("/login", (req, res, next) => {
   //
--- routes/sauces.js
+++ routes/sauces.js
@@ -1,25 +1,30 @@
 //!minOnSave
 const express = require('express');
 const router = express.Router();
-
 const Sauce = require("../models/sauce");
 
-app.get("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
+  Sauce.find()
+  .then((sauces) => res.status(200).json({ sauces }))
+	.catch((error) => res.status(400).json({ error }));
+});
+router.get("/:id", (req, res, next) => {
+  Sauce.findOne({ id: req.params.id })
+  .then((sauce) => res.status(200).json({ sauce }))
+	.catch((error) => res.status(400).json({ error }));
+});
+router.post("/", (req, res, next) => {
   //
 });
-app.get("/:id", (req, res, next) => {
+router.put("/:id", (req, res, next) => {
   //
 });
-app.post("/", (req, res, next) => {
-  //
+router.delete("/:id", (req, res, next) => {
+  Sauce.deleteOne({ id: req.params.id })
+  .then((sauce) => res.status(200).json({ message: "Deleted!" }))
+	.catch((error) => res.status(400).json({ error }));
 });
-app.put("/:id", (req, res, next) => {
-  //
-});
-app.delete("/:id", (req, res, next) => {
-  //
-});
-app.post("/:id/like", (req, res, next) => {
+router.post("/:id/like", (req, res, next) => {
   //
 });
 
