8b42eda4a9ac1cd7c29c235d9913b6239422de35
EmilieGonin <emilie.yuu@gmail.com>
2021-02-15 10:15:00 +0100

    Add token auth middleware

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,13 +1,14 @@
 //!minOnSave
 const express = require('express');
 const router = express.Router();
+const auth = require("../middleware/auth");
 const sauceController = require("../controllers/sauces");
 
 router.get("/", sauceController.getAllSauces);
 router.get("/:id", sauceController.getSauce);
-router.post("/", sauceController.createSauce);
-router.put("/:id", sauceController.updateSauce);
-router.delete("/:id", sauceController.deleteSauce);
-router.post("/:id/like", sauceController.updateLike);
+router.post("/", auth, sauceController.createSauce);
+router.put("/:id", auth, sauceController.updateSauce);
+router.delete("/:id", auth, sauceController.deleteSauce);
+router.post("/:id/like", auth, sauceController.updateLike);
 
 module.exports = router;
