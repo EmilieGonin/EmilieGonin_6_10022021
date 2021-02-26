6b0e18c8aba897eaa10e41b26611e4a1bcbf79c5
EmilieGonin <emilie.yuu@gmail.com>
2021-02-26 09:36:42 +0100

    Add auth to all sauces routes

--- routes/sauces.js
+++ routes/sauces.js
@@ -5,8 +5,8 @@ const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 const sauceController = require("../controllers/sauces");
 
-router.get("/", sauceController.getAllSauces);
-router.get("/:id", sauceController.getSauce);
+router.get("/", auth, sauceController.getAllSauces);
+router.get("/:id", auth, sauceController.getSauce);
 router.post("/", auth, multer, sauceController.createSauce);
 router.put("/:id", auth, multer, sauceController.updateSauce);
 router.delete("/:id", auth, sauceController.deleteSauce);
